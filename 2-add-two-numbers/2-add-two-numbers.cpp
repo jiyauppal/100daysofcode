a41e4d9ebe9df940b9991d3854d7b5c63ef2171c
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-03-10 12:03:21 +0530

    Time: 40 ms (71.08%), Space: 71.4 MB (81.70%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,44 +11,39 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-      if(l1==nullptr && l2==nullptr) return nullptr;
-      ListNode* newHead=nullptr,*curr=nullptr;
-      int carry=0;
-      while(l1 || l2 || carry>0){
-        
-        int sum=0;
-        if(l1){
-          
-          sum+=l1->val;
-          l1=l1->next;
-        }
-        if(l2){
-          sum+=l2->val;
-          l2=l2->next;
-        }
-        if(carry>0) {
-          sum+=carry;
-          carry=0;
-        }
-        
-        if(sum>=10){
-          int rem=sum%10;
-          carry=sum/10;
-          sum=rem;
-        }
-          
-          ListNode* sumNode=new ListNode(sum);
-          if(newHead==nullptr){
-            newHead=sumNode;
+     if(l1==NULL && l2==NULL)
+         return NULL;
+    ListNode* curr = nullptr;
+    ListNode* prev = nullptr;
+    int carry = 0;
+        while(l1 || l2 || carry > 0)
+        {
+            int sum = 0;
+            if(l1){
+            sum+=l1->val;
+            l1 = l1->next;
+            }
+            if(l2){
+            sum+=l2->val;
+            l2 = l2->next;}
+            if(carry){
+            sum+=carry;
+            carry = 0;}
+            if(sum > 9)
+            {
+                int rem = sum%10;
+                carry = sum/10;
+                sum = rem;
+            }
+            ListNode* sumNode = new ListNode(sum);
+            if(prev == nullptr)
+                prev = sumNode;
+            else
+            {
+                curr->next = sumNode;
+            }
+            curr = sumNode;
             
-          }
-          else{
-            curr->next=sumNode;
-          }
-          curr=sumNode;
         }
-        
-      return newHead;
-        
-    }
-};
+        return prev;
+    }};
