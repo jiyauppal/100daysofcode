710ab753b27abd696494b214d17da6fcb480a3e8
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-03-17 23:24:01 +0530

    Time: 5 ms (14.59%), Space: 6.1 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        stack<int> st;
+        int ans = 0;
+        
+        for(auto i : s)
+        {
+            if(i == '(')
+            {
+                st.push(ans);
+			
+                ans = 0;
+            }
+            else 
+            {
+                ans = st.top() + max(ans*2 ,1);
+                st.pop();
+            }
+                
+        }
+        return ans;
+        
+    }
+};
