51a501c255e15ce39421e7a169b5183ed7e58e62
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-04-12 20:01:11 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (84.44%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -4,7 +4,7 @@ public:
     bool isLive(int status){
         return status == 1;
     }
-    int checkCount(int i, int j, vector<vector<int>>& board){
+    int checkCount(int i, int j, vector<vector<int>>& board){  // utility function to keep the count of live neighours
         int count = 0;
         
         count += i-1 >=0 && board[i-1][j] ? 1 : 0;
@@ -22,16 +22,16 @@ public:
         m = board.size();
         n = board[0].size();
         int count;
-        vector<vector<int>> tempboard = board;
+        vector<vector<int>> tempboard = board;  // keeping a temporary so that no final changes are made during each testcase.
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 count = checkCount(i, j, tempboard);
                 if(isLive(board[i][j])){
-                    if(count < 2 || count > 3)
+                    if(count < 2 || count > 3)  // applying condition for live neighours
                         board[i][j] = 0;
                 }
                 else{
-                    board[i][j] = count == 3 ? 1 : 0;
+                    board[i][j] = count == 3 ? 1 : 0; // applying condition for dead neighours
                 }
             }
         }
