48c9b29dc8658c353041f9f4b4d4143c93a2b144
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-04-12 19:54:15 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (20.27%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int m, n;
+    bool isLive(int status){
+        return status == 1;
+    }
+    int checkCount(int i, int j, vector<vector<int>>& board){
+        int count = 0;
+        
+        count += i-1 >=0 && board[i-1][j] ? 1 : 0;
+        count += i+1 < m && board[i+1][j] ? 1 : 0;
+        count += j-1 >=0 && board[i][j-1] ? 1 : 0;
+        count += j+1 < n && board[i][j+1] ? 1 : 0;
+        count += i-1 >=0 && j-1 >= 0 && board[i-1][j-1] ? 1 : 0;
+        count += i-1 >=0 && j+1 < n && board[i-1][j+1] ? 1 : 0;
+        count += i+1 < m && j-1 >= 0 && board[i+1][j-1] ? 1 : 0;
+        count += i+1 < m && j+1 < n && board[i+1][j+1] ? 1 : 0;
+        
+        return count;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        m = board.size();
+        n = board[0].size();
+        int count;
+        vector<vector<int>> tempboard = board;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                count = checkCount(i, j, tempboard);
+                if(isLive(board[i][j])){
+                    if(count < 2 || count > 3)
+                        board[i][j] = 0;
+                }
+                else{
+                    board[i][j] = count == 3 ? 1 : 0;
+                }
+            }
+        }
+    }
+};
