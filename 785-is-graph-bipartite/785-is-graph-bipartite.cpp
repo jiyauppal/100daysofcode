f62750ddbcb07851c1aed8b0d85f307aaf1cdc58
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-04-30 12:59:33 +0530

    Time: 33 ms (49.80%), Space: 13.5 MB (81.90%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& graph, int i, vector<int>& color){
+        if(color[i] == 0) color[i] = 1;
+        
+        for(auto child : graph[i]){
+            if(color[child] == 0){
+                color[child] = -color[i];
+                if(!dfs(graph, child, color)) return false;
+            }
+            else if(color[child] == color[i])
+                return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n, 0);
         for(int i = 0; i < n; i++){
-            if(color[i]) continue;
-            queue<int> q;
-            q.push(i);
-            color[i] = 1;
-            while(!q.empty()){
-                int parent = q.front();
-                q.pop();
-                for(auto child : graph[parent]){
-                    if(color[child] == 0){
-                        color[child] = -color[parent];
-                        q.push(child);
-                    }
-                    else{
-                        if(color[parent] == color[child])
-                            return false;
-                    }
-                }
-            }
+            if(!dfs(graph, i, color))
+                return false;
         }
+    
         return true;
     }
 };
