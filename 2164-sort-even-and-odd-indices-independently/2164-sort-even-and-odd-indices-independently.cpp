8b80c52e764a4793c9613c13d134924194515934
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-05-02 12:43:58 +0530

    Time: 7 ms (79.50%), Space: 11.7 MB (97.69%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        int minIndex;
+// Here in this nested loop , we are sorting the elements at even indices in non-decreasing order.
+        for(int i=0;i<nums.size();i+=2)
+        {
+            minIndex=i;
+            for(int j=i+2;j<nums.size();j+=2)
+            {
+                if(nums[j]<nums[minIndex])
+                    minIndex=j;
+            }
+            swap(nums[i],nums[minIndex]);
+        }
+// Here , we are trying to sort the elements at odd indices in non-increasing order. 
+        for(int i=1;i<nums.size();i+=2)
+        {
+            minIndex=i;
+            for(int j=i+2;j<nums.size();j+=2)
+            {
+                if(nums[j]>nums[minIndex])
+                    minIndex=j;
+            }
+            swap(nums[i],nums[minIndex]);
+        }
+        
+        return nums;
+    }
+};
