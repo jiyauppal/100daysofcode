22b800ee1810d8c5dc8692b4b8a2de142604d51f
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-05-20 01:45:27 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (25.30%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+      int c0 = 0, c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0) c0++;
+            else if(nums[i] == 1) c1++;
+            else if(nums[i] == 2) c2++;
+        }
+        for(int i = 0; i < c0; i++) nums[i] = 0;
+        for(int i = 0; i < c1; i++) nums[i+c0] = 1;
+        for(int i = 0; i < c2; i++) nums[i+c1+c0] = 2;   
+    }
+    
+};
