6175e7e3667e7e2d03b56434b6d5cb61888b9ba1
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-05-20 01:56:58 +0530

    Time: 147 ms (75.17%), Space: 93.3 MB (53.73%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = INT_MAX;
+        int maxpro = 0;
+        for(int i = 0; i < prices.size(); i++){
+            min_price = min(min_price, prices[i]);
+            maxpro = max(maxpro, prices[i] - min_price);
+        }
+        return maxpro;
+    }
+};
