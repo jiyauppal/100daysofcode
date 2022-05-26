f7e419c7980a28dc192290475c812c8bda900dd5
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-05-27 00:20:13 +0530

    Time: 73 ms (23.46%), Space: 19.6 MB (70.55%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+         vector<vector<string>> anagrams;
+        unordered_map<string, vector<string>> mp;
+        for(auto& s : strs){
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        for(auto& p: mp){
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }
+};
