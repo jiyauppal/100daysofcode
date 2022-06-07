21b1c6065dbecaf6c655f924335a3c426eb78b5e
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-08 00:56:43 +0530

    Time: 10 ms (87.01%), Space: 9 MB (78.46%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     
-    int findTargetSumWays(vector<int>& s, int t) {
-        int n=s.size(),l=0;
-        if(s.size()==1)return abs(s[0])==abs(t);
-        for(auto i:s)l+=i; 
-              int k=(l+t)/2;//must watch aditya verma lecture on dp to completely understand it.
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+         if (n == 1) 
+            return abs(nums[0]) == abs(target);
         
-        if((l+t)%2||abs(l)<abs(t))return 0;
+        int s = 0;
+        for(auto i: nums)
+            s += i;
         
-        int dp[n+1][k+1];
-        for(int i=0;i<=n;i++)dp[i][0]=1;//initializing dp row
-        for(int j=1;j<=k;j++)dp[0][j]=0;//initializing dp column
-          for(int i=1;i<=n;i++){
-              for(int j=0;j<=k;j++){
-                   if(s[i-1]<=j)
-                  {
-                      dp[i][j]=dp[i-1][j]+dp[i-1][j-s[i-1]];
-                  }
-                  else
-                      dp[i][j]=dp[i-1][j];
-              }
-          } 
-        return dp[n][k];
+        int sum = (target + s)/2;
+        
+        if ((s + target) % 2 == 1 || abs(s) < abs(target))
+            return 0;
+        
+        int dp[n+1][sum+1];
+
+        for(int i = 0; i < n+1; i++)
+            dp[i][0] = 1;
+            for(int j = 1; j < sum+1; j++)
+                dp[0][j] = 0;
+            
+        for(int i = 1; i < n+1; i++){
+            for(int j = 0; j < sum+1; j++){
+                if(nums[i-1] <= j) dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
     }
 };
