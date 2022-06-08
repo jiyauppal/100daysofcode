9015357d169308450d0f289bdbcf9a22741ab53c
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-08 16:42:05 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -8,7 +8,7 @@ public:
         return true;
     }
     int removePalindromeSub(string s) {
-        if(isPalindromic(s)) return 1;
-        else return 2;
+        if(isPalindromic(s)) return 1; // if string is palindromic it will be deleted in 1 operation
+        else return 2; //if the string is not palindromic there are several a and subsequences which can be deleted in 1 operation
     }
 };
