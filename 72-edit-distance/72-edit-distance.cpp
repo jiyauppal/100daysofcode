bf7f064401f0e24735c883498ba23e27cecf8f52
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-14 16:25:27 +0530

    Time: 28 ms (33.93%), Space: 9 MB (56.01%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //Memoization Approach
+    int solve(string& word1, string& word2, vector<vector<int>>& dp, int i, int j){
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[i] == word2[j]) return dp[i][j] = solve(word1, word2, dp, i-1, j-1);
+        else{
+            return dp[i][j] = 1 + min(solve(word1, word2, dp, i-1, j-1), min(solve(word1, word2, dp, i-1, j), solve(word1, word2, dp, i, j-1)));
+        }
+    }
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>>dp (m, vector<int>(n, -1));
+        return solve(word1, word2, dp,m-1, n-1);
+    }
+};
