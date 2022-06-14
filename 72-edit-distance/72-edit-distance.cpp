a7146bd489f1ca5595c42a741336d78ab340a6ac
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-14 16:40:26 +0530

    Time: 42 ms (24.37%), Space: 8.9 MB (64.24%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    //Memoization Approach
-    int solve(string& word1, string& word2, vector<vector<int>>& dp, int i, int j){
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j]) return dp[i][j] = solve(word1, word2, dp, i-1, j-1);
-        else{
-            return dp[i][j] = 1 + min(solve(word1, word2, dp, i-1, j-1), min(solve(word1, word2, dp, i-1, j), solve(word1, word2, dp, i, j-1)));
-        }
-    }
+    //Tabulation Approach
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>>dp (m, vector<int>(n, -1));
-        return solve(word1, word2, dp,m-1, n-1);
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        for(int i = 0; i <= m; i++) dp[i][0] = i;
+        for(int j = 0; j <= n; j++) dp[0][j] = j;
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] =  dp[i-1][j-1];
+                else{
+            dp[i][j] =  1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
+    }
+        return dp[m][n];
     }
 };
