04baf450a14549970cb3b88ccaad9cb5ad4cbba4
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-16 23:59:50 +0530

    Time: 477 ms (26.27%), Space: 30.1 MB (32.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    string solve(string& s, int i, int j){
-        int n = s.size();
-        while(i>=0 && j <n){
-            if(s[i] != s[j]) break;
-            i--;
-            j++;
-        }
-        return s.substr(i+1, j-1-i);
-    }
-    void maxi(string& fin, string& res, int& maxLen){
-        if(fin.size()>maxLen){
-            res = fin;
-            maxLen = fin.size();
-        }
-    }
     string longestPalindrome(string s) {
-        string res;
-        int maxLen = 0;
-        for(int i = 0; i < s.size(); i++){
-            string odd = solve(s, i, i);
-            maxi(odd,res,maxLen);
-            string even = solve(s, i, i+1);
-            maxi(even,res,maxLen);
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start = 0; int maxLen = 1; //keep two start and maxLen pointers to return substr in results
+        //for substring of length 1(as all are palindromic so)
+        for(int i = 0; i < n; i++)
+            dp[i][i] = true;
+        //for substrings of size 2
+        for(int i = 0; i < n; i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = true;
+                start = i;
+                maxLen = 2;
+            }
         }
-        return res;
+        //for substrings greater than equal size 3
+        for(int k = 3; k <= n; k++) {//here k is size of substring
+            for(int i = 0; i < n-k+1; i++){ //first index of substr
+                int j = i+k-1; //last index of substr
+                if(s[i] == s[j] && dp[i+1][j-1] == true){
+                    dp[i][j] = true;
+                    if(k > maxLen){ //to find longest substring
+                        maxLen = k;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substr(start, maxLen);
     }
 };
