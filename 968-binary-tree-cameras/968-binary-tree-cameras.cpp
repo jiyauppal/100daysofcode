8e7b6fc8ea0042600925c1190a7a7342dad20cd2
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-20 14:31:07 +0530

    Time: 4 ms (98.13%), Space: 21.3 MB (51.41%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -12,22 +12,20 @@
 class Solution {
 public:
     int res = 0;
-    
-    int minCameraCover(TreeNode* root) 
-    {
-        return (dfs(root) < 1 ? 1 : 0) + res;
-    }
-
-    int dfs(TreeNode* root) 
-    {
-        if (!root) return 2;
-        int left = dfs(root->left), right = dfs(root->right);
-        if (left == 0 || right == 0) 
-        {
+    int dfs(TreeNode* root){
+        if(!root) return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        if(left == -1 || right == -1){
             res++;
             return 1;
         }
-        return left == 1 || right == 1 ? 2 : 0;
+        if(left == 1 || right == 1){
+            return 0;
+        }
+        return -1;
     }
-
+    int minCameraCover(TreeNode* root) {
+        return dfs(root)==-1 ? res+1 : res;
+    }
 };
