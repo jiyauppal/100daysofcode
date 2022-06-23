b7063ec47a9ee4b0b7b5b64690cd3b15427ac881
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-23 18:46:10 +0530

    Time: 3 ms (81.05%), Space: 8.2 MB (26.07%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1.size() < str2.size()) return gcdOfStrings(str2, str1);
+        if(str1.size() < str2.size()) return gcdOfStrings(str2, str1); //first string always should be greater than the second one
         else if(str2.empty()) return str1;
-        else if(str1.find(str2) != 0) return "";
+        else if(str1.find(str2) != 0) return ""; // prefix of both strings are same or not if not return empty string
 
         return gcdOfStrings(str1.substr(str2.length()), str2);
     }
