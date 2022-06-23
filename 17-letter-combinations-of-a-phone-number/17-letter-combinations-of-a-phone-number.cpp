74689c5f54693df08196e28a3ed356e3d7937aa5
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-06-23 20:35:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (95.69%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void combinationsLetter(string digits, vector<string>& ans, int index, string output, vector<string>& mapping){
-        if(index >= digits.length()){ ans.push_back(output); return;} // base condition of recursion
-        
-        int number = digits[index] - '0';
-        string value = mapping[number];
-        for(int i = 0; i < value.length(); i++){
-            output.push_back(value[i]);
-            combinationsLetter(digits, ans, index+1, output, mapping);
-            output.pop_back();
+    void combinations(int i, vector<string>& mp, vector<string>& res, string temp, string digits){
+        if(i >= digits.size()) {res.push_back(temp); return;}
+        int number = digits[i] - '0';
+        string value = mp[number];
+        for(int  j= 0; j < value.size(); j++){
+            temp.push_back(value[j]);
+            combinations(i+1,mp, res, temp, digits);
+            temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        int index = 0;
-        if(digits.length() == 0) return ans;
-        string output;
-        vector<string> mapping = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        combinationsLetter(digits, ans, index, output, mapping);
-        return ans;
+        vector<string> mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> res;
+        string temp;
+        if(digits.length() == 0) return res;
+        combinations(0, mp, res, temp, digits);  
+        return res;
     }
 };
