2380ebb53edf038845175fe7c80cea3de811e220
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-07-15 17:02:00 +0530

    Time: 17 ms (93.33%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int dfs(int i, int j, vector<vector<int>> &grid, int n, int m){
-        if(i < 0 || j < 0 || i >= n || j >=m || grid[i][j] == 0) return 0;
-        grid[i][j] = 0;
-        int area = 1;
+        if(i < 0 || j < 0 || i >= n || j >=m || grid[i][j] == 0) return 0; //checking boundaries
+        grid[i][j] = 0; //marking visited
+        int area = 1; //1 because it can't be less than that
         area += dfs(i+1, j, grid, n, m);
         area += dfs(i-1, j, grid, n, m);
         area += dfs(i, j+1, grid, n, m);
@@ -17,7 +17,7 @@ public:
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
-                    ans = max(ans, dfs(i, j, grid, n, m));
+                    ans = max(ans, dfs(i, j, grid, n, m)); // max out of every area
                 }
             }
         }
