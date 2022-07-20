26d77cfe4cd7c19811b0a1dd8477ce2e95fb3d1d
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-07-20 19:26:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-                vector<vector<int>> r(numRows);
-
-        for (int i = 0; i < numRows; i++) {
-            r[i].resize(i + 1);
-            r[i][0] = r[i][i] = 1;
-  
-            for (int j = 1; j < i; j++)
-                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+        vector<vector<int>> ans;
+        int n = numRows;
+        if(n == 1) {
+            ans.push_back({1}); // for edgecase n == 1;
         }
-        
-        return r;
+        if(n == 2){
+            ans.push_back({1});
+            ans.push_back({1, 1}); // for edgecase n == 2;
+        }
+        if(n >= 3){
+            ans.push_back({1}); // will be repeated for all the steps
+            ans.push_back({1, 1});
+            int level = 2;
+            for(int i = 1; i < n-1; ++i){
+                vector<int> v;
+                for(int j = 0; j <= level; ++j){
+                    if(j==0){
+                    v.push_back(ans[i][0]); // first element means row, col = 0;
+                    }
+                    else if(j == level){
+                        v.push_back(ans[i][level-1]); // last element of each row means , col = level
+                    }
+                    else{
+                        v.push_back(ans[i][j-1] + ans[i][j]); // inner elements that will be the sums of above two elements
+                    }
+                }
+                ++level;
+                ans.push_back(v);
+            }
+        }
+        return ans;
     }
 };
