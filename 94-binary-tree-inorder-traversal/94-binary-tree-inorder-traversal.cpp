c905e55128757744d7b3585d045bba06b48476ab
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-07-26 21:43:58 +0530

    Time: 3 ms (59.95%), Space: 8.4 MB (40.38%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,15 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& ans){
+    void traversal(TreeNode* root, vector<int>& ans){
         if(!root) return;
-        helper(root->left, ans);
+        traversal(root->left, ans);
         ans.push_back(root->val);
-        helper(root->right, ans);
+        traversal(root->right, ans);
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        helper(root, ans);
+        traversal(root, ans);
         return ans;
     }
 };
