4e5338fa71eebbcc296df3a6503da4a38cf7d3d2
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-09-04 22:06:13 +0530

    Time: 80 ms (18.16%), Space: 12.3 MB (82.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
+    void nextIsland(vector<vector<char>>& grid, int m, int n, int i, int j){
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        nextIsland(grid, m, n, i+1, j);
+        nextIsland(grid, m, n, i-1, j);
+        nextIsland(grid, m, n, i, j-1);
+        nextIsland(grid, m, n, i, j+1);
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
-        int offsets[] = {0, 1, 0, -1, 0};
         int m = grid.size();
-        int ans = 0;
         int n = grid[0].size();
+        int islands = 0;
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    ans++;
-                    grid[i][j] = '0';
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    while(!q.empty()){
-                        pair<int, int> top = q.front();
-                        q.pop();
-                        for(int k = 0; k < 4; k++){
-                            int r = top.first + offsets[k];
-                            int c = top.second + offsets[k+1];
-                        if(r >= 0 && r < m && c >=0 && c <n && grid[r][c] == '1'){
-                            grid[r][c] = '0';
-                            q.push({r, c});
-                        }}
-                    }
+                    islands++;
+                    nextIsland(grid, m, n, i, j);
                 }
             }
         }
         
-       return ans; 
+        return islands;
     }
 };
