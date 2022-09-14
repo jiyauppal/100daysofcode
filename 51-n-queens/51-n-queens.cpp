417a38d4e3b06084e607394e900bf5e10dbab559
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-09-14 15:49:30 +0530

    Time: 13 ms (46.57%), Space: 7.2 MB (84.88%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
-    void dfs(int col, int n, vector<int>& l, vector<int>& u, vector<int>& ld, vector<vector<string>> &ans, vector<string>& board){
-        if(col == n) {
-        ans.push_back(board);
-        return;}
-        for(int i = 0; i < n; i++){
-            if(l[i] == 0 && ld[i+col] == 0 && u[n-1+col-i] ==0){
-                board[i][col] = 'Q';
-                l[i] = 1;
-                ld[i+col] = 1;
-                u[n-1+col-i] = 1;
-                dfs(col+1, n, l, u, ld, ans, board);
-                board[i][col] = '.';
-                l[i] = 0;
-                ld[i+col] = 0;
-                u[n-1+col-i] = 0;
+    bool isSafe(int i, int j, vector<string>& ds, int n){
+        // if row / col have any Q 
+        int dupi = i;
+        int dupj = j;
+        while(i >= 0 && j >= 0){
+            if(ds[i][j] == 'Q')
+                return false;
+            i--;
+            j--;
+        }
+        i = dupi;
+        j = dupj;
+        while(i >= 0){
+            if(ds[i][j] == 'Q')
+                return false;
+            i--;
+        }
+        i = dupi;
+        j = dupj;
+        while(j < n && i >= 0){
+            if(ds[i][j] == 'Q')
+                return false;
+            i--;
+            j++;
+        }
+        return true;
+    }
+    void NQueens(vector<vector<string>>& results, int n, int i, vector<string>& ds){
+        if(i == n){
+            results.push_back(ds);
+            return;
+        }
+        for(int j = 0; j < n; j++){
+            if(isSafe(i, j, ds, n)){
+            ds[i][j] = 'Q';
+            NQueens(results, n, i+1, ds);
+            ds[i][j] = '.';
             }
         }
-        
-        
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;  //Resultant Matrix of string
-        vector<string> board(n);
+        vector<vector<string>> results;
+        vector<string> ds(n);
         string s(n, '.');
-        for(int i = 0; i < n; i++)
-            board[i] = s;
-       vector<int> l(n, 0), u(2*n-1, 0), ld(2*n-1, 0);
-        dfs(0, n, l, u, ld, ans, board);
-        return ans;
+        for(int i = 0; i < n; i++){
+            ds[i] = s;
+        }
+        NQueens(results, n, 0, ds);
+        return results;
     }
 };
