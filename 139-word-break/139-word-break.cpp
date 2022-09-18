c68507552d67fd323037131874af5c78cedeb742
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-09-18 15:35:28 +0530

    Time: 10 ms (72.61%), Space: 9.6 MB (60.47%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    bool solve(string s, set<string>& dict, int i, vector<char>& dp) {
-        if(i == s.size()) return true;
-        if(dp[i]!=-1) return dp[i];
-        string sub;
+//     bool solve(string s, set<string>& dict, int i, vector<char>& dp) {
+//         if(i == s.size()) return true;
+//         int n = s.size();
+//         if(dp[i]!=-1) return dp[i];
+//         string sub;
         
-        for(int j = i; j < s.size(); j++){
-            if(dict.count(sub+=s[j]) && solve(s, dict, j+1, dp))
-                return dp[i] = 1;;
-        }
-        return dp[i] = 0;
-    }
+//         for(int j = i; j < s.size(); j++){
+//             if(dict.count(sub+=s[j]) && solve(s, dict, j+1, dp))
+//                 return dp[i] = 1;;
+//         }
+//         return dp[n];
+//     }
     bool wordBreak(string s, vector<string>& wordDict) {
         set<string> dict;
-        vector<char> dp(s.size(), -1);
         for(auto i : wordDict){
             dict.insert(i);
         }
-        return solve(s, dict, 0, dp);
+        vector<bool> dp(s.size()+1, false);
+        int n = s.size();
+        dp[0] = true;
+        for(int i = 1; i <= n; i++){
+            for(int j = i-1; j >=0; j--){
+                if(dp[j]){
+                    string sub = s.substr(j, i-j);
+                    if(dict.find(sub) != dict.end()){
+                    dp[i] = true;
+                    break;
+                }}}
+            }
+        return dp[n];
     }
 };
