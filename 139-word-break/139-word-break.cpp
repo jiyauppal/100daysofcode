c095ad6ab04ac4c8eeb2d7117856d90b7cfb6379
Jiya uppal <70507804+jiyauppal@users.noreply.github.com>
2022-09-18 16:04:35 +0530

    Time: 24 ms (45.11%), Space: 8.4 MB (75.69%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-//     bool solve(string s, set<string>& dict, int i, vector<char>& dp) {
-//         if(i == s.size()) return true;
-//         int n = s.size();
-//         if(dp[i]!=-1) return dp[i];
-//         string sub;
-        
-//         for(int j = i; j < s.size(); j++){
-//             if(dict.count(sub+=s[j]) && solve(s, dict, j+1, dp))
-//                 return dp[i] = 1;;
-//         }
-//         return dp[n];
-//     }
     bool wordBreak(string s, vector<string>& wordDict) {
         set<string> dict;
+        int n = s.size();
         for(auto i : wordDict){
             dict.insert(i);
         }
-        vector<bool> dp(s.size()+1, false);
-        int n = s.size();
-        dp[0] = true;
-        for(int i = 1; i <= n; i++){
-            for(int j = i-1; j >=0; j--){
-                if(dp[j]){
-                    string sub = s.substr(j, i-j);
-                    if(dict.find(sub) != dict.end()){
-                    dp[i] = true;
-                    break;
-                }}}
-            }
-        return dp[n];
+        unordered_set<int> vis;
+        queue<int> q({0});
+        while(!q.empty()){
+            int start = q.front();
+            q.pop();
+            if(vis.count(start)) continue; //already visited
+            vis.insert(start); //else insert it into visited set
+            string sub;
+            for(int j = start; j < s.size(); j++)
+                if(dict.count(sub+=s[j])){
+                   q.push(j+1);
+                    if(j+1 == n) return 1;
+                }
+        }
+        return 0;
     }
 };
